5ae319bc2495676a9fdab1b54d19764cd6a3c206
Elton SV <el10savio96@gmail.com>
2022-11-10 10:19:18 +0100

    Update gcd.go

--- gcd/gcd.go
+++ gcd/gcd.go
@@ -6,9 +6,7 @@ import (
 	"golang.org/x/exp/constraints"
 )
 
-var (
-	ErrZeroValues = errors.New("both values cannot be zero")
-)
+var ErrZeroValues = errors.New("both values cannot be zero")
 
 type Integer interface {
 	constraints.Integer
@@ -16,24 +14,30 @@ type Integer interface {
 
 func GCD[T Integer](a, b T) (T, error) {
 	TZero := *new(T)
+
 	if a == TZero && b == TZero {
 		return TZero, ErrZeroValues
 	}
+
 	return gcd(a, b), nil
 }
 
 func gcd[T Integer](a, b T) T {
 	TZero := *new(T)
+
 	if b == TZero {
 		return abs(a)
 	}
+
 	return gcd(b, abs(a)%b)
 }
 
 func abs[T Integer](a T) T {
 	TZero := *new(T)
+
 	if a < TZero {
 		return -a
 	}
+
 	return a
 }
